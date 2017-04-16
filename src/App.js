2ce14dc9edb915a98c05334fadbf68d60e60a1b1
Erica Norby <ericanorby@gmail.com>
2017-04-16 00:29:25 -0400

    added modal window

--- src/App.js
+++ src/App.js
@@ -2,24 +2,80 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  Redirect
+  Link
 } from 'react-router-dom'
+import Modal from 'react-modal'
 
 import Home from './components/Home'
 import Pictures from './components/Pictures'
 import ShowPicture from './components/ShowPicture'
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    }
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   render() {
     return (
       <Router>
         <div>
+
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="uploadPhoto"
+          >
+            <h1>Upload a new picture</h1>
+            <form>
+              <div>
+                <input type="text" placeholder="Photo URL" />
+              </div>
+              <div>
+                <input type="text" placeholder="an alt" />
+              </div>
+              <div>
+                <button type="submit">Upload</button>
+              </div>
+            </form>
+          </Modal>
+
           <header>
-            <Link to="/pictures">
-              <h1>Picaption</h1>
-            </Link>
+            <h1><Link to="/pictures">Picaption</Link></h1>
+            <button id="add-pic-btn" onClick={() => this.openModal()}><span>+</span></button>
           </header>
+
           <main>
             <Route
               exact path="/"
@@ -42,6 +98,7 @@ class App extends Component {
               component={ ShowPicture }
             />
           </main>
+
         </div>
       </Router>
     );
