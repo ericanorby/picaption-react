610ddf42de6beca812e8fdfbbbebd3feec96ae17
Erica Norby <ericanorby@gmail.com>
2017-04-17 14:09:21 -0400

    updated connection

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/picaption-react");
+// mongoose.connect("mongodb://localhost/picaption-react");
+mongoose.connect("mongodb://heroku_97t93bv3:r5rpi43ebdmuslv6rn48hus9om@ds163010.mlab.com:63010/heroku_97t93bv3")
 const db = mongoose.connection;
 
 db.on("error", (err) => {
